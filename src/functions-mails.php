8fb3958e31edc900e6b002d8d7ea1b76b406ee8d
sandraf <contact@sandrafrade.com>
2017-08-11 16:34:43 +0200

    Add doc blocks and fallback for current url

--- src/functions-mails.php
+++ src/functions-mails.php
@@ -2,6 +2,17 @@
 
 namespace Globalis\WP\Cubi;
 
+/**
+ * Send mail wrapped in html tags
+ *
+ * @param string|array $to          Array or comma-separated list of email addresses to send message.
+ * @param string       $subject     Email subject.
+ * @param string       $message     Message contents.
+ * @param string|array $headers     Additional headers.
+ * @param string|array $attachments Files to attach.
+ *
+ * @return bool                     Whether the email contents were sent successfully.
+ */
 function wp_mail_html($to, $subject, $message, $headers = [], $attachments = [])
 {
     $charset = bloginfo('charset');
@@ -10,6 +21,15 @@ function wp_mail_html($to, $subject, $message, $headers = [], $attachments = [])
     wp_mail($to, $subject, $message, $headers, $attachments);
 }
 
+/**
+ * Wrap mail subject and message in html tags
+ *
+ * @param string $subject Email subject.
+ * @param string $message Message contents.
+ * @param string $charset Charset to use.
+ *
+ * @return string         The mail wrapped in html tags
+ */
 function wrap_mail_html($subject, $message, $charset)
 {
     ob_start();
--- src/functions-urls.php
+++ src/functions-urls.php
@@ -2,21 +2,52 @@
 
 namespace Globalis\WP\Cubi;
 
+/**
+ * Get current url with query string or not
+ *
+ * @param  bool   $remove_query_args If true, will remove query string from url.
+ *
+ * @return string $url               The current url.
+ */
 function get_current_url($remove_query_args = false)
 {
-    $url = WP_SCHEME . '://' . WP_DOMAIN . $_SERVER['REQUEST_URI'];
-    if ($remove_query_args) {
-        $parts = parse_url($url);
-        $url   = WP_SCHEME . '://' . WP_DOMAIN . $parts['path'];
+    if (defined(WP_SCHEME) && defined(WP_DOMAIN)) {
+        $url = WP_SCHEME . '://' . WP_DOMAIN . $_SERVER['REQUEST_URI'];
+        if ($remove_query_args) {
+            $parts = parse_url($url);
+            $url   = WP_SCHEME . '://' . WP_DOMAIN . $parts['path'];
+        }
+    } else {
+        $protocol = isset($_SERVER['HTTPS']) ? 'https' : 'http';
+        $url = $protocol . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        if ($remove_query_args) {
+            $parts = parse_url($url);
+            $url   = $protocol . '://' . $_SERVER['HTTP_HOST'] . $parts['path'];
+        }
     }
+
     return $url;
 }
 
+/**
+ * Check if current url starts with given string
+ *
+ * @param  string $search The string searched at the beginning of the current url.
+ *
+ * @return bool           If the current url starts with searched string.
+ */
 function current_url_starts_with($search)
 {
     return str_starts_with(get_current_url(), $search);
 }
 
+/**
+ * Check if current url ends with given string
+ *
+ * @param  string $search The string searched at the end of the current url.
+ *
+ * @return bool           If the current url ends with searched string.
+ */
 function current_url_ends_with($search)
 {
     return str_ends_with(get_current_url(), $search);
