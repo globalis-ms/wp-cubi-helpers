0a0f14fd844b1573210593f97dc3e882a8a71528
Pierre Dargham <pierre.dargham@globalis-ms.com>
2017-08-16 18:28:08 +0200

    Fix get_permalink_by_template(): custom templates work with any post type

--- src/functions-permalinks.php
+++ src/functions-permalinks.php
@@ -12,14 +12,11 @@ namespace Globalis\WP\Cubi;
  */
 function get_permalink_by_template($template, $default = false)
 {
-    static $permalinks;
-    if (!isset($permalinks)) {
-        $permalinks = [];
-    }
+    static $permalinks = [];
     if (!isset($permalinks[$template])) {
         $pages = new \WP_Query([
             'posts_per_page' => 1,
-            'post_type'      => 'page',
+            'post_type'      => 'any',
             'post_status'    => 'publish',
             'meta_key'       => '_wp_page_template',
             'meta_value'     =>  $template,
